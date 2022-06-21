348f18ddca20aff54ef6a3336192e736ae5584ae
cupxu <1706010425@hhu.edu.cn>
2022-06-21 10:33:21 +0800

    测试前后端交互 查询所有用户姓名 接口 ip:8080/user/allUsers

--- src/main/java/com/ff/sxbank/controller/UserController.java
+++ src/main/java/com/ff/sxbank/controller/UserController.java
@@ -10,6 +10,8 @@ import com.ff.sxbank.util.UserInfoUtil;
 import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
